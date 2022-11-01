1966e78f7f5865caed9b87676957e8282f2c57b9
Udaceue <rjemik3@gmail.com>
2022-11-01 19:06:21 +0100

    modified package json and index.js to github pages

--- client/src/index.js
+++ client/src/index.js
@@ -11,7 +11,7 @@ import AboutVip from "./pages/AboutVip";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
     <Routes>
       <Route path="/" element={<App />} />
       <Route path="menu" element={<Menu />} />
